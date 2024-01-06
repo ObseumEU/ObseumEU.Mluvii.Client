3fa8ab057ae947d4010f6ef18a221cf77ae36153
David Rosko <rosko.dav@gmail.com>
2024-01-06 17:33:09 +0100

    Add working example how use it

--- src/ObseumEU.Mluvii.Client.Example/Program.cs
+++ src/ObseumEU.Mluvii.Client.Example/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ObseumEU.Mluvii.Client;
+using ObseumEU.Mluvii.Client.Cache;
+
+// Prepare Dependency Injection and Environment
+var host = Host.CreateDefaultBuilder()
+    .ConfigureAppConfiguration((hostingContext, config) =>
+    {
+        config.SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
+    })
+    .ConfigureServices((hostContext, services) =>
+    {
+        services.Configure<MluviiCredentialOptions>(hostContext.Configuration.GetSection("Mluvii"));
+        services.AddSingleton<ICacheService, InMemoryCache>();
+        services.AddSingleton<ITokenEndpoint, TokenEndpoint>();
+        services.AddSingleton<IMluviiClient, MluviiClient>();
+    })
+    .Build();
+
+// Retrieve and Use mluvii client
+using var serviceScope = host.Services.CreateScope();
+var provider = serviceScope.ServiceProvider;
+
+var mluviiClient = provider.GetRequiredService<IMluviiClient>();
+
+//Call sessions
+var sessions = await mluviiClient.GetSessions(limit: 10);
+Console.WriteLine(string.Join(Environment.NewLine, sessions.value.Select(s => s.Id)));
